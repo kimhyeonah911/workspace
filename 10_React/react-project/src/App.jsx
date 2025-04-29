93bd07e26015354922e9fa31ab86df67b7262477
김현아 <haha020911@naver.com>
2025-04-29 11:30:18 +0900

    react_zustand

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -1,10 +1,43 @@
+import styled from 'styled-components'
 import './App.css'
+import CounterDisplay from './components/CounterDisplay'
+import CounterControls from './components/CounterControls'
+import TodoList from './components/TodoList'
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  width: 100vw;
+  padding: 24px;
+  text-align: center;
+  transition: all 0.3s;
+`
+
+const Section = styled.section`
+  width: 100%;
+  margin: 0 auto;
+  max-width: 800px;
+  padding: 18px;
+  border: 8px;
+  margin-bottom: 20px;
+`
 
 function App() {
   return (
-    <>
-      
-    </>
+    <AppContainer>
+      <Section>
+        <h2>Zustand 전역 상태 관리</h2>
+        <CounterDisplay/>
+        <CounterControls/>
+      </Section>
+      <Section>
+        <h2>Zustane TodoList</h2>
+        <TodoList/>
+      </Section>
+    </AppContainer>
   )
 }
 
---
+++ 10_React/react-project/src/store/useCounterStore.js
@@ -0,0 +1,16 @@
+import { create } from "zustand"
+
+const useCounterStore = create((set, get) => ({
+    count: 0,
+    // set의 콜백으로 상태를 바로 업데이트(불변성 보장)
+    increase: () => set((state) => ({count: state.count + 1})),
+    // get()으로 현재 상태값을 직접 읽어서 업데이트
+    decrease: () => set({count: get().count - 1}),
+    reset: () => set({count: 0}),
+}))
+
+// create(() => {
+//     return 초기값;
+// })
+
+export default useCounterStore
